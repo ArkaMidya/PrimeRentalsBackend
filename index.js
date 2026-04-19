476c10bf6d61b8c0f78a6cb0e2beba7e65b4454b
ArMidya <arkamidya516@gmail.com>
2026-04-19 20:44:20 +0530

    axios

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ app.use('/api/payments', require('./routes/paymentRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
